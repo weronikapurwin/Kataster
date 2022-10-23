2368a0a95c9072e135335f38b46226d2f692a59e
Weronika Purwin <weronikapurwin@wp.pl>
2022-10-23 18:20:01 +0200

    nie dzialaja liczby rzymskie tak mysle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 import numpy as np
 import re
 
@@ -22,16 +23,7 @@ class lexer:
         self.current_char = None
         self.input = source
         self.next_char = self.input.get_char()
-        # self.ofu_r = ['R', 'S', 'Br', 'Wsr', 'W', 'Lzr']   # 23-13/W-RIIIb 23 -> nr obrębu - nr klasy / 
-        # self.ofu_l_ = ['Ł', 'S' 'Br', 'Wsr', 'W', 'Lzr']
-        # self.ofu_ps = ['Ps', 'S' 'Br', 'Wsr', 'W', 'Lzr']
-        # self.ofu_ls = ['Ls', 'W']
-        # self.ofu_lz = ['Lz', 'W']
-
-        # self.ozk_r = ['I', 'II', 'IIIa', 'IIIb', 'IVa', 'IVb', 'V', 'VI', 'VIz']
-        # self.ozk_l_ps_ls_lz = ['I', 'II', 'III','IV', 'V', 'VI']
-
-        self.EOT = -1
+        self.EOT = -2
 
     def _get_next_char(self):
             self.current_char = self.next_char
@@ -41,20 +33,39 @@ class lexer:
                 self.next_char = None
 
     def _build_number(self):
-        pass
+        value = ""
+        if self.current_char.isdigit():
+            if self.current_char == "0":
+                if self.next_char.isdigit():
+                    print(f"No digits allowed after 0 in number ")
+                    return -1
+                return '0'
+            else:
+                while self.next_char and self.next_char.isdigit():
+                    value += self.current_char
+                    self._get_next_char()
+                value += self.current_char
+            return value
  
     def build_token(self):
-
         self._get_next_char()
-        if self.current_char == '-':
+
+        if self.current_char == None:
+            return self.EOT
+
+        if self.current_char in ['-', '/', ' ', '\t']:
             return self.current_char
 
+        t = self._build_number()
+        if t:
+            return t
+            
         if self.current_char == 'K' or self.current_char == 'N':
             if self.next_char == None:
                 return self.current_char
             else:
                 print(f'{self.input.get_string()} OFU = K co tu robi {self.next_char} hmmm? powinien byc koniec stringa')
-                return
+                return -1
 
         #  przypadki z R i Ł
         if self.current_char in ['R', 'Ł'] :
@@ -62,7 +73,7 @@ class lexer:
                 return self.current_char
             else:
                 print(f'{self.input.get_string()} OFU = OZU co tu robi {self.next_char} hmmm?')
-                return
+                return -1
         # przypadki Z W
         if self.current_char == 'W':
             if self.next_char == '-':
@@ -75,24 +86,24 @@ class lexer:
                         return 'Wsr'
                     else:
                         print(f'{self.input.get_string()} OFU to tylko Wsr a ty co tworzysz?')
-                        return
+                        return -1
                 if self.next_char == None:
                     return 'Ws'
                 else:
                     print(f'{self.input.get_string()} coś nie dziala z Ws')
-                    return
+                    return -1
             if self.next_char in ['m', 'p']:
                 return 'W' + self.next_char
             else:
                 print(f'{self.input.get_string()} OFU /= OZU co tu robi {self.next_char} hmmm? powinien być myślnik')
-                return
+                return -1
         # przypadek z S
         if self.current_char == 'S':
             if self.next_char == '-':
                 return self.current_char
             else:
                 print(f'{self.input.get_string()} OFU /= OZU co tu robi {self.next_char} hmmm? powinien być myślnik')
-                return
+                return -1
         # przypadki z B
         if self.current_char == 'B':
             if self.next_char == None:
@@ -103,7 +114,7 @@ class lexer:
                     return 'Br'
                 else:
                     print(f'{self.input.get_string()} powinno być Br')
-                    return
+                    return -1
             # else:
             #         print(f'{self.input.get_string()} OFU /= OZU co tu robi {self.next_char} jest tylko OFU = Br')
             if self.next_char in ['a', 'i', 'p', 'z']:
@@ -112,10 +123,10 @@ class lexer:
                     return 'B' + self.current_char
                 else:
                     print(f'{self.input.get_string()} powinno być Ba bi bp lub bz')
-                    return
+                    return -1
             else:
                 print(f'{self.input.get_string()} jakiś blad z B')
-                return
+                return -1
         # przypadek z Ps
         if self.current_char == 'P':
             if self.next_char == 's':
@@ -124,11 +135,11 @@ class lexer:
                     return 'Ps'
                 else:
                     print(f'{self.input.get_string()} cos po Ps nie gra')
-                    return
+                    return -1
   
             else:
                 print(f'{self.input.get_string()} OFU /= OZU co tu robi {self.next_char} jest tylko OFU = Ps')
-                return
+                return -1
         # przypadki z Ls
         if self.current_char == 'L':
             if self.next_char == 's':
@@ -137,11 +148,11 @@ class lexer:
                     return 'Ls'
                 else:
                     print(f'{self.input.get_string()} powinno po Ls byc nic lub liczba rzymska')
-                    return
+                    return -1
 
             else:
                 print(f'{self.input.get_string()} OFU = OZU co tu robi {self.next_char} powinno być samo Ls')
-                return
+                return -1
         # przypadki z Lz i Lzr
         if self.current_char == 'L':
             if self.next_char == 'z':
@@ -152,29 +163,40 @@ class lexer:
                         return 'Lzr'
                     else:
                         print(f'{self.input.get_string()} powinno być Lzr')
-                        return
+                        return -1
                 if self.next_char in [None, 'I', 'V']:
                     return 'Lz' 
                 else:
                     print(f'{self.input.get_string()} powinno być Lz')
-                    return
+                    return -1
             else:
                 print(f'{self.input.get_string()} cos pojebane z ofu Lz lub Lzr ma byc')
-                return
+                return -1
         # przypadek z dr
         if self.current_char == 'd':
             if self.next_char == 'r':
                 return 'dr'
             else:
                  print(f'{self.input.get_string()} cos pojebane z dr')
-                 return
+                 return -1
         # przypadek z T
         if self.current_char == 'T':
             if self.next_char in ['k', 'i', 'p', 'r']:
                 return 'T' + self.next_char
             else:
                 print(f'{self.input.get_string()} cos pojebane z T')
-                return 
+                return -1
+        else:
+            print(f'{self.input.get_string()} cos  zle')
+            return -1
+    
+    # do testowania
+    def get_all_tokens(self):
+        t = self.build_token()
+        while t != self.EOT:
+            yield t
+            t = self.build_token()
+        yield t
 
 if __name__ == "__main__":
     data = np.array([])
@@ -190,11 +212,16 @@ if __name__ == "__main__":
         if len(x) > 4 and len(x) < 20:
             data1 = np.append(data1, x)
 
-    data2 = ['R', 'RV', 'N', 'N', 'LsIII' ,'PsIII']
-    for i in data2:
+    data2 = ['23-28/E-W']
+    count = 0
+    for i in data1:
         inp = get_input(i)
         lex = lexer(inp)
-        print(lex.build_token())
+        for token in lex.get_all_tokens():
+            if token == -1:
+                count+=1
+            # print(token)
+    print('liczba błędów: ' + str(count))        
 
     
         
